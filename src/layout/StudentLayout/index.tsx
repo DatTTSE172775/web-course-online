a0da7fa034e99cde59e11c4fcd3df90b526bdb75
DatTTSE172775 <datttse172775@fpt.edu.vn>
2024-10-09 13:49:38 +0700

    Setup layout student enhanced

--- src/layout/StudentLayout/index.tsx
+++ src/layout/StudentLayout/index.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState } from "react";
+import { ReactNode } from "react";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import StudentSidebar from "../../components/StudentComponents/StudentSidebar";
@@ -10,25 +10,11 @@ interface StudentLayoutProps {
 export const StudentLayout = ({
   children,
 }: StudentLayoutProps): JSX.Element => {
-  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
-
-  const toggleSidebar = () => {
-    setIsSidebarCollapsed(!isSidebarCollapsed);
-  };
-
   return (
     <div className="flex">
-      {/* Sidebar */}
-      <div className={`${isSidebarCollapsed ? "w-20" : "w-60"} transition-all`}>
-        <StudentSidebar />
-      </div>
-
-      {/* Main content */}
+      <StudentSidebar />
       <div className="flex-1">
-        <Header
-          isSidebarCollapsed={isSidebarCollapsed}
-          onToggleSidebar={toggleSidebar}
-        />
+        <Header />
         <div className="p-5">{children}</div>
       </div>
       <Footer />
